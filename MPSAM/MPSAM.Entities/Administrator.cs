9d4fcbb35f035b1794b6abc7182d3a2422db6835
laviniajurj <lavinia.jurj@yahoo.com>
2021-04-21 22:02:27 +0300

    Add Login and Admin Table

--- MPSAM/MPSAM.Entities/Administrator.cs
+++ MPSAM/MPSAM.Entities/Administrator.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace MPSAM.Entities
 {
-    public class Class1
+    public class Administrator
     {
+        public int ID { get; set; }
+        public string Utilizator { get; set; }
+        public string Parola { get; set; }
     }
 }
---
+++ MPSAM/MPSAM.Web/Controllers/PacientController.cs
@@ -0,0 +1,41 @@
+﻿using MPSAM.Database;
+using MPSAM.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MPSAM.Web.Controllers
+{
+    public class PacientController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(PacientLoginViewModels model)
+        {
+            using (var context = new DBContext())
+            {
+                bool isValid = context.Pacients.Any(x => x.Email == model.Email && x.Parola == model.Password);
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Email, false);
+                    return RedirectToAction("Dashboard", "Pacient");
+                }
+                ModelState.AddModelError("", "Emailul sau parola sunt invalide");
+
+                return View();
+            }
+        }
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
