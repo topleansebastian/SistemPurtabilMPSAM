e7e675241528752c6ed323d1805889091d04d4b8
laviniajurj <lavinia.jurj@yahoo.com>
2021-04-28 21:31:52 +0300

    CRUD - Admin and Dashboard Doctors

--- MPSAM/MPSAM.Web/Controllers/AdministratorController.cs
+++ MPSAM/MPSAM.Web/Controllers/AdministratorController.cs
@@ -45,6 +45,7 @@ namespace MPSAM.Web.Controllers
             var doctors = DoctorServices.ClassObject.GetAllTheDoctors();
             return View(doctors);
         }
+        [HttpGet]
         public ActionResult DoctorsTable(string search, int? pageNumber)
         {
             DoctorSearchViewModel model = new DoctorSearchViewModel();
@@ -89,6 +90,51 @@ namespace MPSAM.Web.Controllers
             DoctorServices.ClassObject.SaveDoctor(newDoctor);
 
             return RedirectToAction("DoctorsTable");
+        }
+        [HttpGet]
+        public ActionResult EditDoctor(int ID)
+        {
+            EditDoctorViewModel model = new EditDoctorViewModel();
+
+            var doctor = DoctorServices.ClassObject.GetDoctor(ID);
+            model.ID = doctor.ID;
+            model.Nume = doctor.Nume;
+            model.Prenume = doctor.Prenume;
+            model.Email = doctor.Email;
+            model.Telefon = doctor.Telefon;
+            model.CodParafa = doctor.CodParafa;
+            model.Cabinet = doctor.Cabinet;
+            model.Parola = doctor.Parola;
+            return PartialView(model);
+        }
+        [HttpPost]
+        public ActionResult EditDoctor(EditDoctorViewModel model)
+        {
+            var existingDoctor = DoctorServices.ClassObject.GetDoctor(model.ID);
+            existingDoctor.Nume = model.Nume;
+            existingDoctor.Prenume = model.Prenume;
+            existingDoctor.Email = model.Email;
+            existingDoctor.Telefon = model.Telefon;
+            existingDoctor.CodParafa = model.CodParafa;
+            existingDoctor.Cabinet = model.Cabinet;
+            existingDoctor.Parola = model.Parola;
+
+            DoctorServices.ClassObject.UpdateDoctor(existingDoctor);
+
+            return RedirectToAction("DoctorsTable");
+        }
+        [HttpPost]
+        public ActionResult DeleteDoctor(int ID)
+        {
+            DoctorServices.ClassObject.DeleteDoctor(ID);
+            return RedirectToAction("DoctorsTable");
+        }
+        [HttpGet]
+        public ActionResult InfoDoctor(int ID)
+        {
+            InfoDoctorViewModel model = new InfoDoctorViewModel();
+            model.Doctor = DoctorServices.ClassObject.GetDoctor(ID);
+            return View(model);
         }
     }
 }
--- MPSAM/MPSAM.Web/ViewModels/AdministratorViewModels.cs
+++ MPSAM/MPSAM.Web/ViewModels/AdministratorViewModels.cs
@@ -11,20 +11,5 @@ namespace MPSAM.Web.ViewModels
         public string Utilizator { get; set; }
         public string Password { get; set; }
     }
-    public class DoctorSearchViewModel
-    {
-        public List<Doctor> Doctors { get; set; }
-        public string WordToBeSearched { get; set; }
-        public Pagination Pagination { get; set; }
-    }
-    public class NewDoctorViewModel
-    {
-        public string Nume { get; set; }
-        public string Prenume { get; set; }
-        public string Email { get; set; }
-        public string Telefon { get; set; }
-        public string CodParafa { get; set; }
-        public string Cabinet { get; set; }
-        public string Parola { get; set; }
-    }
+   
 }
