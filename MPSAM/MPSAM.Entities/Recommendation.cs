36171d6e380b784e170af009e6d3d7b7fa46d051
laviniajurj <lavinia.jurj@yahoo.com>
2021-05-08 13:41:06 +0300

    Add Recom Table

---
+++ MPSAM/MPSAM.Entities/Recommendation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPSAM.Entities
+{
+    public class Recommendation
+    {
+        public int ID { get; set; }
+        public int IDPacient { get; set; }
+        public int IDDoctor { get; set; }
+        public DateTime Data { get; set; }
+        public string Text { get; set; }
+    }
+}
