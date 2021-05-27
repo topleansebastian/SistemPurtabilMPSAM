8d6bbf70caf6d5f791e0ab43c63278347e3b45df
laviniajurj <lavinia.jurj@yahoo.com>
2021-05-27 13:28:40 +0300

    nou

---
+++ MPSAM/MPSAM.Database/Migrations/202105271016527_Descr.cs
@@ -0,0 +1,18 @@
+﻿namespace MPSAM.Database.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Descr : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.ActivityJournals", "Descriere", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.ActivityJournals", "Descriere");
+        }
+    }
+}
